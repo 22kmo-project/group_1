682ff9196eb07d2915a2bd017c271c95a8430cf5
Sampo Sarlin <t1sasa06@students.oamk.fi>
2022-11-20 15:35:38 +0200

    backendiin tehty crudit ja melko lailla valmis

---
+++ backend/models/card_model.js
@@ -0,0 +1,30 @@
+const db = require('../database');
+
+//TODO: CARD_NUMBER --> ID:KSI? TOKENIT, LOGIN-ENDPOINT
+
+const card = {
+    getById: function(id, callback) {
+      return db.query('select * from card where card_number = ?', [id], callback);
+    },
+    getAll: function(callback) {
+      return db.query('select * from card', callback);
+    },
+    add: function(card, callback) {
+      return db.query(
+        'insert into card (card_number, pin_code, debit_credit, id_account, card_owner) values(?,?,?,?,?)',
+        [card.card_number, card.pin_code, card.debit_credit, card.id_account, card.card_owner],
+        callback
+      );
+    },
+    delete: function(id, callback) {
+      return db.query('delete from card where card_number = ?', [id], callback);
+    },
+    update: function(id, card, callback) {
+      return db.query(
+        'update card set pin_code = ? where card_number = ?',
+        [card.pin_code, id],
+        callback
+      );
+    }
+  };
+  module.exports = card;
